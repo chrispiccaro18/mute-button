27874806f00057448a1178e91060522900387997
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-17 16:26:15 -0800

    Not able to toggle mute button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hi');
+const speakerIcon = document.getElementById('speaker-icon');
+const playIcon = document.getElementById('play-icon');
+const audio = new Audio('./assets/audio/judy.mp3');
+audio.muted = false;
+
+playIcon.addEventListener('click', function() {
+    audio.play();
+    speakerIcon.src = './assets/images/mute.png';
+    speakerIcon.alt = 'Mute button';
+    speakerIcon.addEventListener('click', function() {
+        audio.muted = true;
+        speakerIcon.src = './assets/images/speaker.png';
+        speakerIcon.alt = 'Un-mute button';
+    });
+});
